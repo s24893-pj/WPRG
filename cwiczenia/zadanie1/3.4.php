d8d2a415d6530835dc8847eeda83198fc7d259fd
s24893-pj <92208911+s24893-pj@users.noreply.github.com>
2023-03-24 23:20:29 +0100

    Add files via upload

---
+++ cwiczenia/zadanie1/3.4.php
@@ -0,0 +1,19 @@
+<?php
+function czy_pierwsza($n) {
+    if($n<2)
+        return false;
+
+    for($i=2;$i*$i<=$n;$i++)
+        if($n%$i==0)
+            return false;
+    return true;
+}
+
+$n = readline("Podaj liczbę: ");
+
+if(czy_pierwsza($n))
+    echo "Liczba $n jest pierwsza" . PHP_EOL;
+else
+    echo "Liczba $n nie jest pierwsza" . PHP_EOL;
+
+?>
